e35de0d16b24d9d9e1cd0e1eb3873802422d38b8
hardik  khimsuriya <98964297+BEASTXHARDIK@users.noreply.github.com>
2024-01-10 09:15:06 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYHMKPROJECT
+{
+    internal class Myproject
+    {
+        static void Main(string[] args)
+        {
+            int k, i, number = 5,count=5,countabc=5,countdesc=0,counthybrid=5;
+            for (k = 1; k <= number; k++)
+            {
+                
+                for (i = 1; i <= count; i++)
+                    Console.Write(" ");
+                count--;
+                for (i = 1; i <= k ; i++)
+                    Console.Write(i +" ");
+                Console.WriteLine();
+
+            }
+            Console.WriteLine();
+            for (k = 1; k <= number; k++)
+            {
+                
+                for (i = 1; i <= countabc; i++)
+                    Console.Write(" ");
+                countabc--;
+                for (i = 1; i <= k; i++)
+                    Console.Write((Convert.ToChar(i + 64)) + " ");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            for (k = 5; k > 0; k--)
+            {
+                
+                for (i = 5; i > countdesc; i--)
+                    Console.Write(" ");
+                countdesc++;
+                for (i = 5; i >= k; i--)
+                    Console.Write(i + " ");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            for (k = 1; k < 5; k++)
+            {
+
+                for (i = 1; i <= countabc; i++)
+                    Console.Write(" ");
+                countabc--;
+
+                for (i = 1; i <= k; i++)
+                    Console.Write((Convert.ToChar(i + 64)) + " ");
+                    
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            for (k = 1; k < 5; k++)
+            {
+
+                for (i = 1; i <=counthybrid; i++)
+                    Console.Write(" ");
+                counthybrid--;
+
+                for (i = 1; i <= k; i++)
+                    if (k % 2 != 0) { Console.Write(i + " "); }
+                    else { Console.Write((Convert.ToChar(i + 64)) + " "); }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
